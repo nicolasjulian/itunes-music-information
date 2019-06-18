1e574ced51a3d695a282a5e3b273eebc92bbcd33
Nicolas Julian <33948000+nicolasjulian@users.noreply.github.com>
2019-06-18 20:03:44 +0700

    Add Serach by Country

--- iTunes.sh
+++ iTunes.sh
@@ -5,32 +5,55 @@
 #API from RSS Apple Music
 
 clear
-echo "Getting Information From Apple Music...."
+echo "Getting Information From iTunes Music...."
 sleep 5
 clear
+read -p "Seach in country [ID|US|UK|etc.] : " country
+
 cat <<EOF
-	github@nicolasjulian
+
+      \_/       
+     (* *)        
+    __)#(__
+   ( )...( )(_)
+   || |_| ||//
+>==() | | ()/
+    _(___)_
+   [-]   [-]github@nicolasjulian
 --------------------------------------------
 	 [+]iTunes Music Release Tracker[+]
-          	1.top-songs
-          	2.hot-tracks
+	       	1.top-songs
+	       	2.hot-tracks
+	       	3.new-music
+	       	4.recent-release
 --------------------------------------------
 
 EOF
-read -p "Search in cathegory [1|2]: " pilih
+read -p "Search in cathegory [1|2|3|4]: " pilih
 if [[ $pilih == "1" ]]; then
-	echo "Find from Top-Songs Apple Music"
-	url="https://rss.itunes.apple.com/api/v1/us/apple-music/top-songs/all/100/explicit.json"
+	echo "Find from Top-Songs iTunes Music"
+	url="https://rss.itunes.apple.com/api/v1/$country/itunes-music/top-songs/all/100/explicit.json"
 	else
 		if [[ $pilih == "2" ]]; then
-			echo "Find from New-Release Apple Music"
-			url="https://rss.itunes.apple.com/api/v1/us/apple-music/hot-tracks/all/100/explicit.json"
+			echo "Find from Hot-Tracks iTunes Music"
+			url="https://rss.itunes.apple.com/api/v1/$country/itunes-music/hot-tracks/all/100/explicit.json"
 		else
-			exit
+			if [[ $pilih == "3" ]]; then
+				echo "Find from New-Release iTunes Music"
+				url="https://rss.itunes.apple.com/api/v1/$country/itunes-music/new-music/all/100/explicit.json"
+				else
+					if [[ $pilih == "4" ]]; then
+						echo "Find from Recent Release iTunes Music"
+						url="https://rss.itunes.apple.com/api/v1/$country/itunes-music/recent-releases/all/100/explicit.json"
+					else
+						exit
+					fi
+					
+			fi
 		fi
 fi
 
-req=`curl -s $url | jq '.feed.results[] | .artistName + " - " +  .name'`
+req=`curl -s $url | jq '.feed.results[] | .artistName + " - " +  .name + " + Relase [" + .releaseDate + "]"'`
 
 read -p "Specific Artis [type 'n' to display all music list] : "  artis
 if [[ artis == "" ]]; then
